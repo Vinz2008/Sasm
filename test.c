bae0e7b5d588c9112361beb28754b8adf098e0ad
Vinz2008 <clubfunreading@gmail.com>
2022-01-12 16:54:21 +0100

    fixed test.c

--- test.c
+++ test.c
@@ -5,6 +5,8 @@ int main() {
     char sample[20] = "global start";
     int a = startswith("global", sample);
     int b = startswith("globl", sample);
+    printf("a: %i",a);
+    printf("b: %i",b);
     if (a == 1 && b == 0) {
         printf("Success");
     }
